0756755cb4fb68f501d6cd245aeadbef8f70bc67
Louise Black <2129047B@student.gla.ac.uk>
2016-02-05 19:53:25 +0000

    Chapter 6 finished

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return HttpResponse("Rango says here is the about page.")
-	
+
