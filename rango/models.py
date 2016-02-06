91358c4734ae09c2f3afd86c5841f54ce9b9dfe7
Louise Black <2129047B@student.gla.ac.uk>
2016-02-06 20:40:50 +0000

    Chapter 7 guff

--- rango/models.py
+++ rango/models.py
@@ -1,13 +1,16 @@
 from __future__ import unicode_literals
-
+from django.template.defaultfilters import slugify
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    likes = models.IntegerField(default = 0)
-    views = models.IntegerField(default = 0)
-
-    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    def __unicode__(self):
         return self.name
 
 class Page(models.Model):
