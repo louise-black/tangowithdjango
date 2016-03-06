0207a68d794b9c4dd37640913fe472ceb6a45adf
Louise Black <2129047B@student.gla.ac.uk>
2016-03-06 22:37:49 +0000

    Chapter 13- Bootstrap fail

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,12 +18,19 @@ from django.contrib import admin
 from django.conf.urls import patterns
 from django.conf.urls import include
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
 
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+        #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
